f224e36b0fc3aafe1310597f94f13d0362343783
Paul Ursby <paulursby@hotmail.com>
2025-02-28 15:03:18 +0100

    Put Whole Document into Prompt

--- PaulCV_QAbot/paul_CV_QAbot.py
+++ PaulCV_QAbot/paul_CV_QAbot.py
@@ -1,16 +1,22 @@
 # Play around with RAG and Agent using Langchain framework
 
 """Required packages
-pip install langchain-community pypdf
+# pip install langchain-community pypdf
 pip install langchain-community pymupdf
+pip install langchain-openai
 """
 
 import os
-from pprint import pprint
+# from pprint import pprint
 
 # from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
+
+################
 # Load Documents
 ################
 
@@ -30,5 +36,50 @@ all_pages = [
 ]
 
 # Print results
-print(f"Loaded {len(all_pages)} documents.\n")
-pprint(all_pages[:])
+# print(f"Loaded {len(all_pages)} documents.\n")
+# pprint(all_pages[:])
+
+################################
+# Put Whole Document into Prompt
+################################
+
+# Function to create an OpenAI GPT-4o model
+def llm_model():
+    llm = ChatOpenAI(
+        model_name="gpt-4o",  # Use GPT-4o model
+        temperature=0.5,  # Controls creativity
+        max_tokens=256  # Limits response length
+    )
+    return llm
+
+# Initialize GPT-4o
+openai_llm = llm_model()
+
+# response = openai_llm.invoke("How are you?")
+# print(response.content)
+
+# Create prompt template
+template = """According to the document content here 
+            {content},
+            answer this question 
+            {question}.
+            Do not try to make up the answer.
+                
+            YOUR RESPONSE:
+"""
+prompt_template = PromptTemplate(template=template, input_variables=['content', 'question'])
+
+# Create a runnable chain
+query_chain = prompt_template | openai_llm | RunnableLambda(lambda x: x.content)
+
+# Example input
+data = {
+    "content": all_pages,
+    "question": "What does Paul like with Volvo Cars?"
+}
+
+# Invoke the chain
+response = query_chain.invoke(data)
+
+# Print the response
+print(response)
