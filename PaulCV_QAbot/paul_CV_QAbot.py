962f914561345c279b33935d996722475ee7c540
Paul Ursby <paulursby@hotmail.com>
2025-02-28 10:02:31 +0100

    Add Document loader for multiple PDF files

--- PaulCV_QAbot/paul_CV_QAbot.py
+++ PaulCV_QAbot/paul_CV_QAbot.py
@@ -5,16 +5,30 @@ pip install langchain-community pypdf
 pip install langchain-community pymupdf
 """
 
+import os
 from pprint import pprint
 
-# from pathlib import Path
 # from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyMuPDFLoader
+
+# Load Documents
+################
 
 # loader = PyPDFLoader(file_path="./RAG_data/CV General Paul C.pdf")
 # pages = loader.load_and_split()
 
-loader = PyMuPDFLoader(file_path="./RAG_data/CV General Paul C.pdf")
-pages = loader.load()
+# loader = PyMuPDFLoader(file_path="./RAG_data/CV General Paul C.pdf")
+# pages = loader.load()
 
-pprint(pages[0])
+# Directory containing PDFs
+pdf_directory = "./RAG_data/"
+
+# Load all PDFs
+all_pages = [
+    page for pdf in os.listdir(pdf_directory) if pdf.endswith(".pdf")
+    for page in PyMuPDFLoader(os.path.join(pdf_directory, pdf)).load()
+]
+
+# Print results
+print(f"Loaded {len(all_pages)} documents.\n")
+pprint(all_pages[:])
