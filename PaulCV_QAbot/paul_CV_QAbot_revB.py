796c743773b0f1133b5a8c73bc3c5da8d346ad29
Paul Ursby <paulursby@hotmail.com>
2025-03-17 18:33:12 +0100

    Add Gradio interface web GUI and logging

--- PaulCV_QAbot/paul_CV_QAbot_revB.py
+++ PaulCV_QAbot/paul_CV_QAbot_revB.py
@@ -5,11 +5,13 @@ pip install langchain-community pymupdf
 pip install langchain-openai
 pip install langchain-text-splitters
 pip install chromadb langchain-chroma
+pip install gradio
 """
 
 import logging
 import os
 
+import gradio as gr
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.retrievers.multi_query import MultiQueryRetriever
@@ -20,6 +22,24 @@ from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+
+# Configure logging for all LangChain components
+logging.basicConfig(level=logging.INFO)
+for component in [
+    "langchain",
+    "langchain.llms",
+    "langchain.chains",
+    "langchain.agents",
+    "langchain.memory",
+    "langchain.tools",
+    "langchain.prompts",
+    "langchain.callbacks",
+    "langchain.vectorstores",
+    "langchain.retrievers",
+    "langchain.document_loaders",
+]:
+    logging.getLogger(component).setLevel(logging.INFO)
 
 
 # Create an OpenAI GPT-4o model
@@ -42,9 +62,7 @@ def document_loader(rag_doc_dir):
         for page in PyMuPDFLoader(os.path.join(rag_doc_dir, pdf)).load()
     ]
 
-    # Print results
-    # print(f"Loaded {len(all_pages)} documents.\n")
-    # pprint(all_pages[:])
+    logging.info(f"Loaded {len(loaded_pages)} documents.")
     return loaded_pages
 
 
@@ -61,9 +79,9 @@ def text_splitter(data):
     # Flatten the list if you want a single list of all chunks
     flatten_chunks = [chunk for sublist in orig_chunks for chunk in sublist]
 
-    print(f"\nSplitted data - no of chunks: {len(flatten_chunks)}")
-    print(f"Splitted data - type: {type(flatten_chunks)}")
-    print(f"Splitted data element - type: {type(flatten_chunks[0])}")
+    logging.info(f"Splitted data - no of chunks: {len(flatten_chunks)}")
+    logging.info(f"Splitted data - type: {type(flatten_chunks)}")
+    logging.info(f"Splitted data element - type: {type(flatten_chunks[0])}")
     return flatten_chunks
 
 
@@ -83,10 +101,10 @@ def vector_database(chunks):
 
     # Create an ID list that will be used to assign each chunk a known unique identifier
     ids = [str(i) for i in range(0, len(chunks))]
-
     # Store text chunks in Vector DB
     vector_db.add_texts(chunks, ids=ids)
-    print(f"\nNumber of added data chunks to Chroma vector DB: {len(ids)}")
+
+    logging.info(f"Number of added data chunks to Chroma vector DB: {len(ids)}")
     return vector_db
 
 
@@ -100,9 +118,6 @@ def retriever(rag_doc_dir, openai_llm):
     retriever = MultiQueryRetriever.from_llm(
         retriever=vector_db.as_retriever(), llm=openai_llm
     )
-
-    logging.basicConfig()
-    logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
     return retriever
 
 
@@ -125,10 +140,41 @@ def retriever_qa_chain(rag_doc_dir, query, openai_llm):
     chain = create_retrieval_chain(retriever_qa, question_answer_chain)
 
     response = chain.invoke({"input": query})
-    print(f"\nResponse from Q&A chain: {response['answer']}")
+    logging.info(f"Response from Q&A chain: {response['answer']}")
+    return response["answer"]
+
+
+def retriever_qa_chain_interface(file, query):
+    openai_llm = create_openai_llm_model()
+    response = retriever_qa_chain(file, query, openai_llm)
     return response
 
 
+# Create Gradio interface
+rag_application = gr.Interface(
+    fn=retriever_qa_chain_interface,
+    allow_flagging="never",
+    inputs=[
+        # gr.File(label="Upload PDF File", file_count="single", file_types=[".pdf"], type="filepath"),
+        gr.Textbox(
+            label="Path to RAG data files", value="./RAG_data/CV_Qabot/", lines=1
+        ),  # Default value added
+        gr.Textbox(
+            label="Input Query", lines=2, placeholder="Type your question here..."
+        ),
+    ],
+    outputs=gr.Textbox(
+        label="Output", placeholder="Chatbot answer will appear here..."
+    ),
+    title="Pauls CV RAG Chatbot",
+    description="Add the path to the RAG data file(s) and ask any question. The chatbot will try to answer using the provided document.",
+)
+
+# Launch the app
+rag_application.launch(server_name="0.0.0.0", server_port=7860)
+
+
+""" Running application without GUI
 # OpenAI GPT-4o model
 openai_llm = create_openai_llm_model()
 
@@ -137,6 +183,7 @@ rag_doc_dir = "./RAG_data/CV_Qabot/"
 
 # User query
 query = "What does Paul like with Volvo Cars?"
-# query = "How many years of experince does Paul have from telecom Industry?"
+# query = "How many years of experience does Paul have from telecom Industry?"
 
 response = retriever_qa_chain(rag_doc_dir, query, openai_llm)
+"""
